588f476c10686cac60450e1634a1d5d06674ba92
Nitish Kumar <Nitishx13@gmail.com>
2025-11-17 01:40:33 +0530

    first commit
    first commit

---
+++ header.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Digintag | Best Digital Wedding Invitation Designer | Premium E-Invitations</title>
+    <meta name="description" content="Digintag | Best Digital Wedding Invitation Designer in Delhi NCR & Mumbai brings you online digital video invitation designs and makes your life-events a memorable wealth for generation.">
+    <meta name="author" content="Digintag">
+
+    <!-- Favicons-->
+    <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+    <link rel="apple-touch-icon" type="image/x-icon" href="img/apple-touch-icon-57x57-precomposed.png">
+    <link rel="apple-touch-icon" type="image/x-icon" sizes="72x72" href="img/apple-touch-icon-72x72-precomposed.png">
+    <link rel="apple-touch-icon" type="image/x-icon" sizes="114x114" href="img/apple-touch-icon-114x114-precomposed.png">
+    <link rel="apple-touch-icon" type="image/x-icon" sizes="144x144" href="img/apple-touch-icon-144x144-precomposed.png">
+
+    <!-- GOOGLE WEB FONT -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    
+    <!-- FONT AWESOME -->
+    <link rel="stylesheet" href="wedding.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+
+    <!-- BASE CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/vendors.css" rel="stylesheet">
+    
+    <!-- BLACK & MONOCHOME COLOR PALETTE -->
+    <link href="css/black-colors.css" rel="stylesheet">
+
+    <!-- YOUR CUSTOM CSS -->
+    <link href="css/custom.css" rel="stylesheet">
+
+</head>
+<body>
+
+<div id="preloader">
+    <div data-loader="circle-side"></div>
+</div><!-- /Preload -->
+
+<div class="min-vh-100 d-flex flex-column">
+
+    <header>
+        <div class="container-fluid">
+            <div class="container-wrapper">
+                <div class="row d-flex align-items-center">
+                    <div class="col-4">
+                        <a data-bs-toggle="offcanvas" href="#offcanvasNav" role="button" class="btn_nav"><i class="bi bi-list"></i></a>
+                    </div>
+                    <div class="col-4 text-center">
+                        <a href="index.html"><img src="img/logo.svg" alt="Digintag" class="img-fluid" width="95" height="30"></a>
+                    </div>
+                    <div class="col-4">
+                        <div id="social">
+                            <ul>
+                                <li><a href="#0"><i class="bi bi-facebook"></i></a></li>
+                                <li><a href="#0"><i class="bi bi-twitter-x"></i></a></li>
+                                <li><a href="#0"><i class="bi bi-instagram"></i></a></li>
+                            </ul>
+                        </div>
+                        <!-- /social -->
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- /container -->
+    </header>
+    <!-- /header -->
+
+    <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasNav">
+        <div class="offcanvas-header">
+            <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        </div>
+        <div class="offcanvas-body">
+            <ul>
+                <li><a href="index.html" class="animated_link">Home</a></li>
+                <li><a href="wedding.html" class="animated_link active">Wedding Invitations</a></li>
+                <li><a href="index.html#services" class="animated_link">Services</a></li>
+                <li><a href="about.html" class="animated_link">About</a></li>
+                <li><a href="contact.html" class="animated_link">Contact</a></li>
+            </ul>
+        </div>
+    </div>
+    <!-- /offcanvas nav -->
